f936e8c18a2638da3f935f6409e2b6bbeb9de554
amayapowerdot <128687924+amayapowerdot@users.noreply.github.com>
2023-06-14 15:43:53 +0600

    Update installPrometheusConverter.sh

--- installPrometheusConverter.sh
+++ installPrometheusConverter.sh
@@ -3,22 +3,22 @@
 # Создаем директорию
 mkdir -p /go/PrometheusConverter
 
-ENV_REWRITE = false
+ENV_REWRITE=0
 
 # Проверяем на наличие файла .env (если нет, то создаем)
 if [ ! -f /go/PrometheusConverter/.env ]; then
     touch /go/PrometheusConverter/.env
-    ENV_REWRITE = true
+    ENV_REWRITE=1
 else
     echo ".env файл уже существует, желаете перезаписать? (y/n)"
     read answer
 
     if [ "$answer" == "y" ]; then
-        ENV_REWRITE = true
+        ENV_REWRITE=1
     fi
 fi
 
-if [ "$ENV_REWRITE" == true ]; then
+if [ "$ENV_REWRITE" == 1 ]; then
   # Спрашиваем название сервера
   echo "Введите название сервера:"
   read SERVER_NAME
