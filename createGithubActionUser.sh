23686824cc70e47394f71e67276477a5bf3b7e8d
amayapowerdot <128687924+amayapowerdot@users.noreply.github.com>
2023-07-11 11:44:13 +0600

    Update createGithubActionUser.sh

--- createGithubActionUser.sh
+++ createGithubActionUser.sh
@@ -4,8 +4,27 @@ username="githubactions"
 
 # Check if the user already exists
 if id "$username" >/dev/null 2>&1; then
-echo "User $username already exists. Exiting."
-exit 1
+  # ask for a new password
+  echo "User $username already exists. Do yo want to update password?"
+  read -p "Type 'yes' to update password: " answer
+  if [ "$answer" == "yes" ]; then
+    # Generate a random password
+    password=$(openssl rand -base64 32)
+
+    # Update the user with the generated password
+    sudo usermod -p "$(openssl passwd -1 "$password")" "$username"
+
+    # Save password to file
+    echo "$password" > /root/.githubactions
+
+    echo "Password for user $username has been updated with the following password: $password"
+    echo "Password saved at: cat /root/.githubactions"
+
+    exit 0
+  else
+    echo "Password for user $username has not been updated. Cancelled."
+    exit 0
+  fi
 else
 
 # Generate a random password
